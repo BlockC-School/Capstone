db74e9b980c9a91123ac5388bc24cafad9ecd129
Krishna Kant Sharma <31980947+krishna7860@users.noreply.github.com>
2022-04-18 21:21:39 +0530

    Create XRP.sol

---
+++ mocks/XRP.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract XRP is ERC20 {
+    constructor() ERC20('XRP', 'Ripple') public {}
+
+    function faucet(address to, uint amount) external {
+        _mint(to, amount);
+    }
+}
